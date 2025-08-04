91fbf89500e992ceb6608a0712d370664e8ef664
ahmed_ramadan1288 <ahmed_ramadan1288>
2025-08-04 15:36:16 +0300

    Create new project

---
+++ Swift-WebP-ImageDemoTests/Swift_WebP_ImageDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_WebP_ImageDemoTests.swift
+//  Swift-WebP-ImageDemoTests
+//
+//  Created by Apple on 04/08/2025.
+//
+
+import Testing
+@testable import Swift_WebP_ImageDemo
+
+struct Swift_WebP_ImageDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Swift-WebP-ImageDemoUITests/Swift_WebP_ImageDemoUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Swift_WebP_ImageDemoUITestsLaunchTests.swift
+//  Swift-WebP-ImageDemoUITests
+//
+//  Created by Apple on 04/08/2025.
+//
+
+import XCTest
+
+final class Swift_WebP_ImageDemoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
